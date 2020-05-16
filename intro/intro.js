600a026cf3ba78da0cb13308ffc058e3068acf83
pB <webforeman@gmail.com>
2020-05-16 08:30:15 +0300

    Describe and matchers

--- intro/intro.js
+++ intro/intro.js
@@ -1,21 +1,5 @@
-function expect(value) {
-  return {
-    toBe: exp => {
-      if (value === exp) {
-        console.log('Success')
-      } else {
-        console.error(`Value is ${value}, but expectation is ${exp}`)
-      }
-    }
-  }
-}
-
 const sum = (a, b) => a + b
 
 const nativeNull = () => null
-
-console.log(sum(42, 2))
-
-expect(sum(42, 2)).toBe(42)
 
 module.exports = { sum, nativeNull }
